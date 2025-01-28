a36a6e7df8fd9fab2007085e2df87a3304b1c766
ESJ0 <usuarioj0s3p1@gmail.com>
2025-01-27 18:31:09 -0600

    Se creo la clase Stack

---
+++ Stack.java
@@ -0,0 +1,21 @@
+public class Stack implements IStack{
+
+    @Override
+    public void push(int value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public int pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public int operation(char operator, int value1, int value2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'operation'");
+    }
+    
+}
