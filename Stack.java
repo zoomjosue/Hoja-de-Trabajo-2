4bd62861bd4afecce520a59198d5b8271143d4c1
ESJ0 <usuarioj0s3p1@gmail.com>
2025-01-29 18:14:20 -0600

    Se creo el metodo isEmpty para verificar si la stack esta vacia y evitar errores. Ademas se creo el metodo pop que elimina y devuelveel ultimo elemento agregado a la stack

--- Stack.java
+++ Stack.java
@@ -8,10 +8,30 @@ public class Stack<T> implements IStack<T>{
         this.size = 0;
     }
 
+    /**
+     * Este metodo verifica si la stack esta vacia
+     *
+     * @return retorna true si la stack esta vacia, false si no esta vacia
+     */
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    /**
+     * Etse metedo elimina y devuelve el ultimo elemento ingresado a la stack
+     *
+     * @return el elemento eliminado de la pila
+     * @throws IllegalStateException si la pila esta vacaa
+     */
     @Override
     public T pop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+        if (isEmpty()) {
+            throw new IllegalStateException("Brother, el stack esta vacio");
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
     }
 
     @Override
@@ -25,5 +45,5 @@ public class Stack<T> implements IStack<T>{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'push'");
     }
-        
+       
 }
