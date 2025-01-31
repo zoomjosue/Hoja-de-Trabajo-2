c2f3e15b0467e7b9c97ce404c61877be7268ba48
ESJ0 <usuarioj0s3p1@gmail.com>
2025-01-30 19:36:55 -0600

    Se actulizaron los nombres de las variables y clase (Puchis con los demas grupos)

--- TestMetodoCalculator.java
+++ TestMetodoCalculator.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test;
 /**
  * Clase de pruebas unitarias para el metodo en la clase Postfix
  */
-class PostFixTest {
-    private PostFix postFix;
+class TestMetodoCalculadora {
+    private  Calculator calculator;
 
     /**
      * Configura una nueva instancia de Postfix antes de cada prueba
      */
     @BeforeEach
     void setUp() {
-        postFix = new PostFix();
+        calculator = new Calculator();
     }
 
     /**
@@ -21,9 +21,9 @@ class PostFixTest {
      */
     @Test
     void testEvaluatePostFix() {
-        assertEquals(9, postFix.evaluatePostFix("4 5 +"), "4 + 5 debería ser 9");
-        assertEquals(20, postFix.evaluatePostFix("4 5 *"), "4 * 5 debería ser 20");
-        assertEquals(2, postFix.evaluatePostFix("8 4 /"), "8 / 4 debería ser 2");
-        assertEquals(3, postFix.evaluatePostFix("10 7 -"), "10 - 7 debería ser 3");
+        assertEquals(9, calculator.evaluatePostFix("4 5 +"), "4 + 5 debería ser 9");
+        assertEquals(20, calculator.evaluatePostFix("4 5 *"), "4 * 5 debería ser 20");
+        assertEquals(2, calculator.evaluatePostFix("8 4 /"), "8 / 4 debería ser 2");
+        assertEquals(3, calculator.evaluatePostFix("10 7 -"), "10 - 7 debería ser 3");
     }
 }
